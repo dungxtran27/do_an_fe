40ac8fa51bbdee6057e986de70ad82ac55b93dd2
mrchuu <chuquyson4@gmail.com>
2024-10-06 23:13:26 +0700

    ix/FSPAR-43: task ux

--- src/component/student/DashBoard/Task/index.tsx
+++ src/component/student/DashBoard/Task/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { TASK_FILTERS, TASK_STATUS_FILTER } from "../../../../utils/const";
-import { Button, Form, Input, Select, SelectProps, Tooltip } from "antd";
+import { Button, Form, Input, Select, Tooltip } from "antd";
 import styles from "./styles.module.scss";
 import FormItem from "antd/es/form/FormItem";
 import { SearchOutlined } from "@ant-design/icons";
@@ -8,7 +8,7 @@ import { FaFileExcel } from "react-icons/fa6";
 import TaskBoard from "./TaskBoard";
 import classNames from "classnames";
 import CreateTask from "./CreateTask";
-type LabelRender = SelectProps["labelRender"];
+// type LabelRender = SelectProps["labelRender"];
 const Task = () => {
   const [status, setStatus] = useState<string>("All");
   const [memberSearch, setMemberSearch] = useState<string>("");
@@ -42,7 +42,6 @@ const Task = () => {
   const [form] = Form.useForm();
   // const taskType = Form.useWatch(TASK_FILTERS.taskType, form);
   // const assignee = Form.useWatch(TASK_FILTERS.assignee, form);
-  const timeBlock = Form.useWatch(TASK_FILTERS.timeBlock, form);
   // const searchKey = Form.useWatch(TASK_FILTERS.searchKey, form);
 
   const taskFilter = () => {
@@ -53,46 +52,7 @@ const Task = () => {
       { value: "hieu", label: "hieu" },
       { value: "thang", label: "thang" },
     ];
-    const timeblocks = [
-      {
-        label: "Outcome 1",
-        value: "outcome1",
-        color: "#fb923c",
-      },
-      {
-        label: "Outcome 2",
-        value: "outcome2",
-        color: "#f472b6",
-      },
-      {
-        label: "Outcome 3",
-        value: "outcome3",
-        color: "#60a5fa",
-      },
-    ];
 
-    const labelRender: LabelRender = (props) => {
-      const selectedTimeBlockColor = timeblocks.find(
-        (tb) => tb.value === timeBlock
-      )?.color;
-      const { label } = props;
-      if (label) {
-        return (
-          <div className="flex items-center gap-3 rounded-md h-full w-full">
-            <span
-              className="w-5 aspect-square rounded-full"
-              style={{ backgroundColor: selectedTimeBlockColor }}
-            ></span>
-            <Tooltip title={`${label}(28/09/2024 - 23/10/2024)`}>
-              <span className="truncate max-w-[100px]">
-                {label}(28/09/2024 - 23/10/2024)
-              </span>
-            </Tooltip>
-          </div>
-        );
-      }
-      return <span>No option match</span>;
-    };
     return (
       <Form
         className="flex items-center gap-4 mt-5"
@@ -130,24 +90,6 @@ const Task = () => {
             )}
           />
         </FormItem>
-        <FormItem name={TASK_FILTERS.timeBlock} label={"Time block"}>
-          <Select
-            allowClear
-            size="middle"
-            style={{ width: 180 }}
-            options={timeblocks}
-            optionRender={(op) => (
-              <div className="flex items-center gap-3 rounded-md h-full w-full">
-                <span
-                  className="w-5 aspect-square rounded-full"
-                  style={{ backgroundColor: op.data.color }}
-                ></span>
-                <span>{op.data.label}</span>
-              </div>
-            )}
-            labelRender={labelRender}
-          />
-        </FormItem>
         <FormItem name={TASK_FILTERS.searchKey} label={"Search"}>
           <Input
             suffix={<SearchOutlined />}
@@ -162,50 +104,24 @@ const Task = () => {
     {
       key: "1",
       taskType: "Group Task",
-      taskKey: "SE-01",
       name: "Tạo timeline cho marketing",
       assignee: {
         color: "#e11d48",
         name: "Sơn Chu",
       },
       status: "In Progress",
-      timeblock: {
-        color: "#a3e635",
-        name: "Project init",
-      },
       dueDate: new Date(),
     },
     {
       key: "2",
       taskType: "Class Task",
-      taskKey: "SE-02",
       name: "Outcome 2",
       assignee: {
         color: "#fb923c",
         name: "Trần Dũng",
       },
       status: "Need Review",
-      timeblock: {
-        color: "#fb923c",
-        name: "Outcome 2",
-      },
     },
-    // {
-    //   key: '1',
-    //   taskType: 'Group Task',
-    //   taskKey: 'SE-02',
-    //   name: 'Tạo timeline cho marketing',
-    //   assignee: {
-    //     color: '#e11d48',
-    //     name: 'Sơn Chu'
-    //   },
-    //   status: 'In Progress',
-    //   timeblock: {
-    //     color: '#a3e635',
-    //     name: 'Project init'
-    //   },
-    //   dueDate: new Date(),
-    // }
   ];
   return (
     <div>
